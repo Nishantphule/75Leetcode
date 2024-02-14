84277af721c13687d102e60a30677c467d415485
Nishantphule <nishantphule12@gmail.com>
2024-02-15 00:16:55 +0530

    Day-13/100-prob 13/75 leetcode

---
+++ problem13.js
@@ -0,0 +1,31 @@
+// max number of k sum pairs
+
+var maxOperations = function (nums, k) {
+
+    nums = nums.sort((a, b) => a - b)
+
+    let count = 0;
+
+    let left = 0;
+
+    let right = nums.length - 1;
+
+    while (left < right) {
+
+        if (nums[left] + nums[right] === k) {
+
+            count++;
+
+            left++
+            right--
+
+        } else if (nums[left] + nums[right] < k) {
+
+            left++
+
+        } else right--
+
+    }
+
+    return count
+};
